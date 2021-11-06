5e7ff23ec407246a6bdc071e804a1a742d038a22
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-11-06 17:34:40 -0300

    created Category model

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import { categoriesRoutes } from './routes/categories.routes';
 const app = express();
 
 app.use(express.json());
-app.use(categoriesRoutes);
+app.use('/categories', categoriesRoutes);
 
 app.get('/', (_, response) => response.json({ message: 'Hello World!' }));
 
