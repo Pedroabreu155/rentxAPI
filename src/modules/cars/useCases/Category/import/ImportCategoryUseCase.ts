f556b443812b129b7f339227ef3cb41d053e5ca6
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-11-08 20:08:11 -0300

    created readFile stream and parse with node and csv-parse

--- src/modules/cars/useCases/Category/import/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/Category/import/ImportCategoryUseCase.ts
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import csvParse from 'csv-parse';
+
 export class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on('data', async line => {
+      console.log(line);
+    });
   }
 }
