0900fcc270595e35bb17b77617a2c880aec62f5b
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-11-14 22:39:12 -0300

    change Category reposisoty with typeorm methods

--- src/modules/cars/repositories/Categories/CategoriesRepository.ts
+++ src/modules/cars/repositories/Categories/CategoriesRepository.ts
@@ -1,13 +1,14 @@
+import { getRepository, Repository } from 'typeorm';
 import { Category } from '../../entities/Category';
 import { ICreateCategoryDTO, ICategoriesRepository } from './interfaces';
 
 export class CategoriesRepository implements ICategoriesRepository {
-  private categories: Category[];
+  private repository: Repository<Category>;
 
   private static INSTANCE: CategoriesRepository;
 
   private constructor() {
-    this.categories = [];
+    this.repository = getRepository(Category);
   }
 
   public static getInstance(): CategoriesRepository {
@@ -17,25 +18,22 @@ export class CategoriesRepository implements ICategoriesRepository {
     return CategoriesRepository.INSTANCE;
   }
 
-  create({ name, description }: ICreateCategoryDTO): void {
-    const category = new Category();
-
-    Object.assign(category, {
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const category = this.repository.create({
       name,
       description,
-      created_at: new Date(),
     });
 
-    this.categories.push(category);
+    await this.repository.save(category);
   }
 
-  list(): Category[] {
-    return this.categories;
+  async list(): Promise<Category[]> {
+    const categories = await this.repository.find();
+    return categories;
   }
 
-  findByName(name: string): Category {
-    const category = this.categories.find(category => category.name === name);
-
+  async findByName(name: string): Promise<Category> {
+    const category = await this.repository.findOne({ name });
     return category;
   }
 }
--- src/modules/cars/repositories/Categories/interfaces.ts
+++ src/modules/cars/repositories/Categories/interfaces.ts
@@ -6,7 +6,7 @@ export interface ICreateCategoryDTO {
 }
 
 export interface ICategoriesRepository {
-  create({ name, description }: ICreateCategoryDTO): void;
-  list(): Category[];
-  findByName(name: string): Category;
+  create({ name, description }: ICreateCategoryDTO): Promise<void>;
+  list(): Promise<Category[]>;
+  findByName(name: string): Promise<Category>;
 }
